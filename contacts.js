23bf80784187a3e9ffd68440a1c955b15198b079
Ilya Bondarchuk <cod3provider@gmail.com>
2024-05-03 12:34:00 +0200

    added try... catch syntax for errors handling

--- contacts.js
+++ contacts.js
@@ -6,41 +6,60 @@ const contactsPath = path.resolve('db', 'contacts.json');
 const updateContacts = contacts => fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
 export async function listContacts() {
-  const contacts = await fs.readFile(contactsPath);
-  return JSON.parse(contacts);
+  try {
+    const contacts = await fs.readFile(contactsPath);
+    return JSON.parse(contacts);
+  }
+  catch (err) {
+    console.log(err.message);
+  }
 }
 
 export async function getContactById(contactId) {
-  const contacts = await listContacts();
-  const foundContact = contacts.find(contact => contact.id = contactId);
-  return foundContact || null;
+  try {
+    const contacts = await listContacts();
+    const foundContact = contacts.find(contact => contact.id = contactId);
+    return foundContact || null;
+  }
+  catch (err) {
+    console.log(err.message);
+  }
 }
 
 export async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const idx = contacts.findIndex(contact => contact.id === contactId);
+  try {
+    const contacts = await listContacts();
+    const idx = contacts.findIndex(contact => contact.id === contactId);
 
-  if (idx === -1) {
-    return null;
+    if (idx === -1) {
+      return null;
+    }
+
+    const [deletedContact] = contacts.splice(idx, 1);
+    await updateContacts(contacts);
+
+    return deletedContact;
   }
-
-  const [deletedContact] = contacts.splice(idx, 1);
-  await updateContacts(contacts);
-
-  return deletedContact;
+  catch (err) {
+    console.log(err.message);
+  }
 }
 
 export async function addContact(id, name, email, phone) {
-  // ...твій код. Повертає об'єкт доданого контакту (з id).
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  }
-  contacts.push(newContact);
-  await updateContacts(contacts);
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    }
+    contacts.push(newContact);
+    await updateContacts(contacts);
 
-  return newContact;
+    return newContact;
+  }
+  catch (err) {
+    console.log(err.message);
+  }
 }
